5e3b2cf7464924f32f9a730437c75b643dcede70
Michael Uno <michael@michaeluno.jp>
2015-03-05 10:42:09 +0900

    Fixed an issue that unit tests did not activate the plugin.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -13,3 +13,5 @@ function _manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
+
+activate_plugin( 'sample-test-plugin/sample-test-plugin.php' );
