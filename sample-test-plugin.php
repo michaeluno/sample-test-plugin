c212494ebc43dfad5cf99183b764e9aeb3024b10
Michael Uno <michael@michaeluno.jp>
2015-03-05 13:10:24 +0900

    Added a class so that Scrutinizer will start analyzing the code.

--- sample-test-plugin.php
+++ sample-test-plugin.php
@@ -1,5 +1,20 @@
 <?php
 /* Plugin Name: Sample Test Plugin */
+
 function getSampleTestValue() {
     return true;
+}
+
+class SampleTestPlugin {
+    
+    public function getA() {
+        return 'A';
+    }
+    public function getB() {
+        return 'B';
+    }    
+    public function render() {
+        echo "<p>" . getA() . getB() . "</p>";
+    }
+    
 }
